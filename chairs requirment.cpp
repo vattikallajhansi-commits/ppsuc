fc33dab167a2691170732b37c96ba87d85bf099c
vattikallajhansi13 <vattikallajhansi@gmail.com>
2025-11-30 08:10:16 +0530

    Add files via upload

---
+++ chairs requirment.cpp
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main() {
+    int X, Y;
+    scanf("%d %d", &X, &Y);
+
+    if (X <= Y)
+        printf("0");
+    else
+        printf("%d", X - Y);
+
+    return 0;
+}
+
